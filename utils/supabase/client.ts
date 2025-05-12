d5dd9aa78c9cbb15169b65ab5d15bd106c802749
AngeGAbriel-rgb <gabrielange433@gmail.com>
2025-05-12 13:41:55 +0100

    admin modif

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,7 +1,11 @@
 // utils/supabase/client.ts
-import { createClient } from '@supabase/supabase-js'
+import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createSupabaseClient(supabaseUrl, supabaseKey)
+
+export function createClient() {
+  return createSupabaseClient(supabaseUrl, supabaseKey)
+}
